ba710f3f57777553ded64e74308f6370e7a3af3d
Anand-Patidar03 <anandpatidar2107@gmail.com>
2026-03-25 21:20:05 +0530

    fixed api url

--- src/app.js
+++ src/app.js
@@ -6,7 +6,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://pay-kart-frontend-y6zo.vercel.app"
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
